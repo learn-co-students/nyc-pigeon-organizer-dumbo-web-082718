1bc00fc6c20ad8538d6db24382609d4f88280b3a
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-08-14 11:35:24 -0400

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, value_hash|
+    value_hash.each do |key, value_array|
+      value_array.each do |name|
+        if new_hash[name].nil?
+          new_hash[name] = {}
+          new_hash[name][attribute] = [key.to_s]
+        elsif new_hash[name][attribute].nil?
+          new_hash[name][attribute] = [key.to_s]
+        else
+          new_hash[name][attribute] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
