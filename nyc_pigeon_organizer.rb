ab6c1e8a2a2a33c743c821a1ddafaed48b8451b7
github username <github email address>
2018-08-22 14:03:14 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash={}
+  data[:gender].each do |gender,pigeons|
+    pigeons.each do |pigeon|
+      new_hash[pigeon]= {:color=>[],:gender=>[gender.to_s],:lives=>[]}
+    end
+  end
+  data[:color].each do |color,pigeons|
+    pigeons.each do |pigeon|
+      new_hash[pigeon][:color]<<color.to_s
+    end
+  end
+  data[:lives].each do |place, pigeons|
+    pigeons.each do |pigeon|
+      new_hash[pigeon][:lives]<<place.to_s
+    end
+  end
+  new_hash
 end
